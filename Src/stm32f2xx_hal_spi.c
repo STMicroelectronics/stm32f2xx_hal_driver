24d0cfd8694feb312680e56b3ff9b906b08ba33e
karim <karim.rabhi-ext@st.com>
2026-05-22 15:04:48 +0100

    [HAL][SPI] Fix INTEGER_OVERFLOW Coverity warning

--- Src/stm32f2xx_hal_spi.c
+++ Src/stm32f2xx_hal_spi.c
@@ -3545,7 +3545,10 @@ static HAL_StatusTypeDef SPI_WaitFlagStateUntilTimeout(SPI_HandleTypeDef *hspi, 
       {
         tmp_timeout = 0U;
       }
-      count--;
+      else
+      {
+        count--;
+      }
     }
   }
 
